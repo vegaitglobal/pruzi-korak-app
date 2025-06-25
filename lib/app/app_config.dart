6b74040bf0226b677a374bc2619f6b4bf548f91b
Nemanja Pajic <nemanja.pajic@vegait.rs>
2025-06-25 15:37:50 +0200

    Change notification hour

--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -1,5 +1,5 @@
 
 class AppConfig {
-  static const int motivationNotificationHour = 15;
-  static const int motivationNotificationMinute = 28;
+  static const int motivationNotificationHour = 10;
+  static const int motivationNotificationMinute = 0;
 }
