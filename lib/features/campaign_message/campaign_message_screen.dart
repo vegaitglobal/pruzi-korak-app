96584f626e25abe44a54135dd9bb6f591eb79dae
Nemanja Pajic <nemanja.pajic@vegait.rs>
2025-06-27 13:18:20 +0200

    Connect bloc logic with CampaignMessageScreen

--- lib/features/campaign_message/campaign_message_screen.dart
+++ lib/features/campaign_message/campaign_message_screen.dart
@@ -1,64 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pruzi_korak/app/navigation/app_routes.dart';
 import 'package:pruzi_korak/app/theme/app_text_styles.dart';
 import 'package:pruzi_korak/core/localization/app_localizations.dart';
-import 'package:pruzi_korak/shared_ui/components/app_header.dart';
+import 'package:pruzi_korak/domain/campaign/campaign_message.dart';
+import 'package:pruzi_korak/features/campaign_message/bloc/campaign_message_bloc.dart';
 import 'package:pruzi_korak/shared_ui/components/buttons.dart';
+import 'package:pruzi_korak/shared_ui/components/cached_image.dart';
+import 'package:pruzi_korak/shared_ui/components/error_screen.dart';
+import 'package:pruzi_korak/shared_ui/components/loading_components.dart';
 
-class CampaignMessageScreen extends StatelessWidget {
+class CampaignMessageScreen extends StatefulWidget {
   const CampaignMessageScreen({super.key});
 
   @override
+  State<CampaignMessageScreen> createState() => _CampaignMessageScreenState();
+}
+
+class _CampaignMessageScreenState extends State<CampaignMessageScreen> {
+  @override
   Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
-    const String heading = 'Campaign Message';
-    const String campaignBody =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.";
-    
+    return BlocBuilder<CampaignMessageBloc, CampaignMessageState>(
+      builder: (context, state) {
+        return switch (state) {
+          CampaignMessageInitial() => const AppLoader(),
+          CampaignMessageLoading() => const AppLoader(),
+          CampaignMessageLoaded() => CampaignMessageContent(
+            campaignMessage: state.campaignMessage,
+          ),
+          CampaignMessageError() => ErrorComponent(
+            errorMessage:
+                AppLocalizations.of(context)!.unexpected_error_occurred,
+            onRetry: () {
+              context.read<CampaignMessageBloc>().add(
+                const CampaignMessageLoad(),
+              );
+            },
+          ),
+        };
+      },
+    );
+  }
+}
+
+class CampaignMessageContent extends StatelessWidget {
+  final CampaignMessage campaignMessage;
+
+  const CampaignMessageContent({super.key, required this.campaignMessage});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // Default header
-              const AppHeader(),
-              
-              const SizedBox(height: 32.0),
-              
-              // Heading with font size 24px
-              Text(
-                heading,
-                style: AppTextStyles.titleLarge,
-              ),
-              
-              const SizedBox(height: 16.0),
-              
-              // Long text with font size 12px
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Text(
-                    campaignBody,
-                    style: AppTextStyles.bodySmall,
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50.0),
+                child: Center(
+                  child: CachedImage(
+                    imageUrl: campaignMessage.logoUrl,
+                    height: 40.0,
                   ),
                 ),
               ),
-              
-              const SizedBox(height: 24.0),
-              
-              // Button with primary color and text "continue_to_app"
-              SizedBox(
-                width: double.infinity,
-                child: AppButton(
-                  text: localizations.continue_to_app, 
-                  onPressed: () {
-                    // Navigate to the home screen or any other screen as needed
-                    context.go(AppRoutes.home.path());
-                  },
+
+              Text(
+                campaignMessage.heading,
+                style: AppTextStyles.titleLarge,
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 16.0),
+
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 50.0),
+                  child: SingleChildScrollView(
+                    child: Text(
+                      campaignMessage.message,
+                      style: AppTextStyles.bodySmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
               ),
+
+              const SizedBox(height: 24.0),
+
+              AppButton(
+                text: AppLocalizations.of(context)!.continue_to_app,
+                onPressed: () {
+                  context.go(AppRoutes.home.path());
+                },
+              ),
+              const SizedBox(height: 24.0),
+
             ],
           ),
         ),
