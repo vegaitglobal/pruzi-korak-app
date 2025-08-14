1a3d65ddddd25980e9163996cab2904a5cc91e69
Nemanja Pajic <nemanja.pajic@vegait.rs>
2025-08-14 16:37:43 +0200

    Change target sdk to 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "org.pruzikorak.pruziKorak"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 26
-        targetSdk = 34
+        minSdk = 28
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
