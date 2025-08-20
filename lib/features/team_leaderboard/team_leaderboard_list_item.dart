e922524fb214bc39967a78d17f9551c87c459312
Nemanja Pajic <nemanja.pajic@vegait.rs>
2025-08-20 21:26:18 +0200

    Shows avatars if imageUrl not null

--- lib/features/team_leaderboard/team_leaderboard_list_item.dart
+++ lib/features/team_leaderboard/team_leaderboard_list_item.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:pruzi_korak/app/theme/colors.dart';
 import 'package:pruzi_korak/domain/leaderboard/team_leaderboard_model.dart';
 import 'package:pruzi_korak/shared_ui/components/avatar_with_badge.dart';
+import 'package:pruzi_korak/shared_ui/components/cached_image.dart';
 import 'package:pruzi_korak/shared_ui/components/initials_avatar.dart';
 
 class TeamLeaderboardListItem extends StatelessWidget {
-  const TeamLeaderboardListItem({super.key, required this.teamLeaderboardModel, required this.onItemClick});
+  const TeamLeaderboardListItem({
+    super.key,
+    required this.teamLeaderboardModel,
+    required this.onItemClick,
+  });
 
   final TeamLeaderboardModel teamLeaderboardModel;
   final Function(String) onItemClick;
@@ -13,7 +18,9 @@ class TeamLeaderboardListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final initial =
-        teamLeaderboardModel.teamName.isNotEmpty ? teamLeaderboardModel.teamName[0] : '?';
+        teamLeaderboardModel.teamName.isNotEmpty
+            ? teamLeaderboardModel.teamName[0]
+            : '?';
 
     return InkWell(
       onTap: () => onItemClick(teamLeaderboardModel.teamId),
@@ -24,7 +31,13 @@ class TeamLeaderboardListItem extends StatelessWidget {
             badgeSize: BadgeSize.small,
             badgeValue: teamLeaderboardModel.rank,
 
-            child: InitialsAvatar(initial: initial, size: 42),
+            child:
+                teamLeaderboardModel.imageUrl != null
+                    ? UserAvatarImage(
+                      imageUrl: teamLeaderboardModel.imageUrl!,
+                      size: 42,
+                    )
+                    : InitialsAvatar(initial: initial, size: 42),
           ),
           const SizedBox(width: 16),
           Column(
